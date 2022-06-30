ca9f32ba381b14b93932649d745668b052383c52
shaina <Shainagupta9253@gmail.com>
2022-06-29 23:20:33 -0700

    Added code to get weather data from database

--- com/weatherapp/DataLayer.py
+++ com/weatherapp/DataLayer.py
@@ -1,29 +1,49 @@
-import sqlite3
-
-class WeatherDataLayer:
+import psycopg2
 
 
-    def save_to_db(weather_dict):
-        create_table_query = "create table if not exists weather (city TEXT, state TEXT, country TEXT, " \
-                             "longitude TEXT, latitude TEXT, temperature TEXT, timestamp TEXT)"
-        city = str(weather_dict['city'])
-        state = str(weather_dict['state'])
+def get_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="weatherapp",
+        user="postgres",
+        password="password")
 
-        country = str(weather_dict['country'])
-        latitude = str(weather_dict['latitude'])
-        longitude = str(weather_dict['longitude'])
-        temperature = str(weather_dict['temperature'])
-        timestamp = str(weather_dict['timestamp'])
-        # logic to manipulate data and save it to database.
-        connection = sqlite3.connect("weather.db")
-        # creating connection object
-        cursor = connection.cursor()
-        # creating table
-        connection.execute(create_table_query)
-        connection.execute(
-            """Insert into weather (city, state, country, latitude, longitude, temperature, timestamp) values(?,?,?,?,?,?,?);""",
+    return conn
+
+
+def save_to_db(weather_dict):
+    city = str(weather_dict['city'])
+    state = str(weather_dict['state'])
+
+    country = str(weather_dict['country'])
+    latitude = str(weather_dict['latitude'])
+    longitude = str(weather_dict['longitude'])
+    temperature = str(weather_dict['temperature'])
+    timestamp = str(weather_dict['timestamp'])
+    # logic to manipulate data and save it to database.
+    # creating connection object
+    try:
+        conn = get_connection();
+        cursor = conn.cursor()
+        cursor.execute(
+            '''Insert into weather (city, state, country, latitude, longitude, temperature, timestamp) values(%s,%s,%s,%s,%s,%s,%s);''',
             (city, state, country, latitude, longitude, temperature, timestamp))
         # writing create table query
-        connection.commit()
-        connection.close()
+        conn.commit()
+        conn.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def get_all_from_db():
+    connection=get_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+       'select * from weather');
+    rows= cursor.fetchall()
+    return rows
 
--- com/weatherapp/WeatherController.py
+++ com/weatherapp/WeatherController.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, Response
 import json
-from validator import Validator
-from DataLayer import WeatherDataLayer
+from validator import validate_request
+from DataLayer import save_to_db, get_all_from_db
 
 app = Flask(__name__)
 
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
-    return "yeh lo weather"
 
+    rows= get_all_from_db()
+    rows_json = json.dumps(rows)
+    response = Response(rows_json,content_type="application/json" )
+    return response
 
 @app.route('/weather', methods=['POST'])
 def add_weather():
@@ -17,25 +20,26 @@ def add_weather():
     # got the json data in string in the above line
     weather_dict = json.loads(request_data)
     # validation done here
-    error_fields = Validator.validate_request(weather_dict)
+    error_fields = validate_request(weather_dict)
     if (len(error_fields) > 0):
         error_status = "\"message\":\"{} value is required\"".format(error_fields)
         response = Response(error_status, status=400, content_type="application/json")
         return response
     # valid data, now process it
 
-    WeatherDataLayer.save_to_db(weather_dict)
-
+    save_to_db(weather_dict)
     return "{\"message\":\"weather added successfully\"}"
 
 
 @app.route('/weather', methods=['PUT'])
 def put_weather():
+    #Code to be implemented
     return "weather updated"
 
 
 @app.route('/weather', methods=['DELETE'])
 def delete_weather():
+    #code to be implemented
     return "weather deleted"
 
 
